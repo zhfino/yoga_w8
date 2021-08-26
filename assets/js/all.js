de063faff8acbe70e2b27f055b22b4aeb95e26fa
zhfino <zhfino@gmail.com>
2021-08-26 17:40:53 +0800

    Update 2021-08-26T09:40:44.439Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,30 +1,30 @@
-$(function() {
+"use strict";
+
+$(function () {
   console.log('Hello Bootstrap5');
 });
-
 var swiper = new Swiper(".myRecSwiper", {
   slidesPerView: 3,
   grid: {
-    rows: 2,
+    rows: 2
   },
-    autoplay: {
+  autoplay: {
     delay: 2500,
-    disableOnInteraction: false,
+    disableOnInteraction: false
   },
   spaceBetween: 16,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
+    clickable: true
+  }
 });
-
-
 var swiper = new Swiper(".myLesSwiper", {
   slidesPerView: 1.05,
   spaceBetween: 3,
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
+    clickable: true
+  }
 });
+//# sourceMappingURL=all.js.map
