0dbb640e3a12477bb270d5c7eac1c57ac540ca49
zhfino <zhfino@gmail.com>
2021-08-26 17:39:44 +0800

    index/image update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,38 +2,29 @@ $(function() {
   console.log('Hello Bootstrap5');
 });
 
-
-
-var mySwiper = new Swiper ( ".swiper-container", {
-  // 參數設定[註1]
-  direction: "vertical", // 方向
-  loop: true,  // 循環
-  pagination: {
-    el: ".swiper-pagination",  // 分頁物件
+var swiper = new Swiper(".myRecSwiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
   },
-  navigation: {
-    nextEl: ".swiper-button-next", // 上一頁按鈕物件
-    prevEl: ".swiper-button-prev", // 下一頁按鈕物件
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar", // 捲軸物件
-  }
-})
-
-
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
+    autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
+  spaceBetween: 16,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+});
+
+
+var swiper = new Swiper(".myLesSwiper", {
+  slidesPerView: 1.05,
+  spaceBetween: 3,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
